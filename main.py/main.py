44b3d8da2bd67feec59bd766b758fba5113274ad
Jinx <JeniceJinx@users.noreply.github.com>
2022-10-24 08:22:51 -0400

    added more code for minimum and maximum bets and lines to bet on

--- main.py/main.py
+++ main.py/main.py
@@ -1,5 +1,7 @@
 from curses.ascii import isdigit
-
+MAX_LINES = 3
+MAX_BET =100
+MIN_BET = 1
 
 def deposit():
     while True:
@@ -9,4 +11,27 @@ def deposit():
             if amount > 0:
                 break
             else: 
-                print("Amount must begreater than o")
+                print("Amount must be greater than 0")
+        else:
+            print("Please enter a number.")
+    return amount
+
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number of lines to bet on (1- " + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else: 
+                print("Enter a valid number of lines between 1 and 3")
+        else:
+            print("Please enter a number.")
+    return lines
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    print(balance,lines)
+
+main()
